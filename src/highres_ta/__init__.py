9d4cf7c8630cfd1cdfd419eb150f0a7797b3578a
lukegre <lukegre@gmail.com>
2026-04-17 15:20:41 +0200

    Added LinearCatBooster

--- src/highres_ta/__init__.py
+++ src/highres_ta/__init__.py
@@ -1,2 +1,7 @@
+from highres_ta.estimators import BaggingCatBoostResidualRegressor, CatBoostResidualRegressor
+
+__all__ = ["CatBoostResidualRegressor", "BaggingCatBoostResidualRegressor", "main"]
+
+
 def main() -> None:
     print("Hello from highres-ta!")
